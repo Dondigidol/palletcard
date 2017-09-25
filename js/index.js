a44bfeb9dd86f22c17238149602cd2990dcbd4fa
dondigidol <pillar_box@bk.ru>
2017-09-25 20:08:35 +0300

    написал обработчик авторизации

--- js/index.js
+++ js/index.js
@@ -1,5 +1,30 @@
 // Нажатие кнопки логина =================
-function loginbtn_clk(){
-	alert("Нажата кнопка входа");
+function loginbtn_clk(){	
+	$("#alerts").html('');
+	var ldap = $("#login").val();
+	var ldappw = $("#password").val();
+	
+	if ((ldap != "") && (ldappw != "")){
+		
+		$.ajax({
+			type: 'POST',
+			url: 'aj/login.php',
+			data: {'user': ldap, 'userPW': ldappw},
+			success: function(data) {
+				if (data == "true")
+				{
+					location.replace("./card.php");		
+				}
+				else
+				{
+					$("#alerts").css("visibility", "visible");
+					$("#alerts").html(data);
+				}
+			}
+		});
+	} else {
+		$("#alerts").css("visibility", "visible");
+		$("#alerts").html("Внимание! Заполните все поля.");
+	}
 }
 //----------------------------------------
